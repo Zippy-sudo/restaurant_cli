f56c7881d55ce9b86082fafd767ede4cc57a0ea8
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-20 19:12:04 +0300

    MVP

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,7 @@
 import sqlite3
 
-CONN = sqlite3.connect('company.db')
+CONN = sqlite3.connect("db/restaurant_company.db")
 CURSOR = CONN.cursor()
+
+def create_conn():
+    return sqlite3.connect("db/restaurant_company.db")
