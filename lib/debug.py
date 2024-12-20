baf780a2716d87886c3394ec974417991612e597
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-20 19:17:09 +0300

    Setup Debug

--- lib/debug.py
+++ lib/debug.py
@@ -2,6 +2,9 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.restaurant import Restaurant
+from models.worker import Worker
+from models.shift import Shift
 import ipdb
 
 
